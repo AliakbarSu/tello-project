3f97eaf22bda2d95b29461095827b2aa78998200
AliakbarSu <aliakbar.su@hotmail.com>
2021-10-18 12:13:33 +1300

    Use react-p5 instead of external script

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,6 +6,15 @@ import Throutles from './Throutle'
 import VideoContainer from './VideoContainer'
 import FlightButtons from './FlightButtons'
 import Training from './Training'
+import Sketch from 'react-p5'
+
+let mobilenet
+let classifier
+let video
+let label = ''
+let detector
+let results = []
+
 let socket = io('ws://' + document.location.hostname + ':3000', {
   reconnectionDelayMax: 10000
 })
@@ -64,8 +73,84 @@ function App(props) {
     classifier.save()
   }
 
+  function whileTraining(loss) {
+    if (loss === null) {
+      console.log('training completed')
+      classifier.classify(gotResults)
+    } else {
+      console.log(loss)
+    }
+  }
+
+  function gotResults(error, result) {
+    if (error) {
+      console.log('Error thrown', error)
+    } else {
+      // console.log(result)
+      label = result[0].label
+      classifier.classify(gotResults)
+    }
+  }
+
+  function detectionResults(error, res) {
+    if (error) {
+      console.log('Error thrown', error)
+    } else {
+      results = res
+    }
+    detector.then((rs) => rs.detect(video, detectionResults))
+  }
+
+  function modelReady() {
+    console.log('model is ready!!')
+  }
+
+  function customModelReady() {
+    classifier.classify(gotResults)
+  }
+
+  function videoReady() {
+    console.log('video is ready!!')
+  }
+
+  function preload() {
+    detector = ml5.objectDetector('cocossd')
+  }
+
+  const setup = (p5, parent) => {
+    p5.createCanvas(700, 600).parent('canvasForHTML')
+    // canvas.parent('canvasForHTML')
+
+    video = p5.createCapture(p5.VIDEO)
+    video.hide()
+    p5.background(0)
+    mobilenet = ml5.featureExtractor('MobileNet', { numLabels: 7 }, modelReady)
+    classifier = mobilenet.classification(video, videoReady)
+
+    // Detect image
+    detector.then((rs) => rs.detect(video, detectionResults))
+  }
+  const draw = (p5) => {
+    p5.image(video, 0, 0, 700, 600)
+    p5.fill(173, 255, 47)
+    p5.textSize(30)
+    p5.text(label, 10, p5.height - 20)
+
+    results.forEach((res) => {
+      p5.stroke(255, 255, 0)
+      p5.strokeWeight(3)
+      p5.noFill(0)
+      p5.rect(res.x, res.y, res.width, res.height)
+      p5.noStroke()
+      p5.fill(0)
+      p5.textSize(24)
+      p5.text(res.label, res.x + 12, res.y + 24)
+    })
+  }
+
   return (
     <div className="tello">
+      <Sketch setup={setup} draw={draw} preload={preload} />
       <Training
         battery={battery}
         status={status}
--- server/public/script.js
+++ server/public/script.js
@@ -1,86 +1,86 @@
-let mobilenet
-let classifier
-let video
-let label = ''
-let detector
-let results = []
+// let mobilenet
+// let classifier
+// let video
+// let label = ''
+// let detector
+// let results = []
 
-let leftSide
-let rightSide
-let trainButton
+// let leftSide
+// let rightSide
+// let trainButton
 
-function whileTraining(loss) {
-  if (loss === null) {
-    console.log('training completed')
-    classifier.classify(gotResults)
-  } else {
-    console.log(loss)
-  }
-}
+// function whileTraining(loss) {
+//   if (loss === null) {
+//     console.log('training completed')
+//     classifier.classify(gotResults)
+//   } else {
+//     console.log(loss)
+//   }
+// }
 
-function gotResults(error, result) {
-  if (error) {
-    console.log('Error thrown', error)
-  } else {
-    // console.log(result)
-    label = result[0].label
-    classifier.classify(gotResults)
-  }
-}
+// function gotResults(error, result) {
+//   if (error) {
+//     console.log('Error thrown', error)
+//   } else {
+//     // console.log(result)
+//     label = result[0].label
+//     classifier.classify(gotResults)
+//   }
+// }
 
-function detectionResults(error, res) {
-  if (error) {
-    console.log('Error thrown', error)
-  } else {
-    results = res
-  }
-  detector.then((rs) => rs.detect(video, detectionResults))
-}
+// function detectionResults(error, res) {
+//   if (error) {
+//     console.log('Error thrown', error)
+//   } else {
+//     results = res
+//   }
+//   detector.then((rs) => rs.detect(video, detectionResults))
+// }
 
-function modelReady() {
-  console.log('model is ready!!')
-}
+// function modelReady() {
+//   console.log('model is ready!!')
+// }
 
-function customModelReady() {
-  classifier.classify(gotResults)
-}
+// function customModelReady() {
+//   classifier.classify(gotResults)
+// }
 
-function videoReady() {
-  console.log('video is ready!!')
-}
+// function videoReady() {
+//   console.log('video is ready!!')
+// }
 
-function preload() {
-  detector = ml5.objectDetector('cocossd')
-}
+// function preload() {
+//   detector = ml5.objectDetector('cocossd')
+// }
 
-function setup() {
-  canvas = createCanvas(700, 600)
-  canvas.parent('canvasForHTML')
+// function setup() {
+//   canvas = createCanvas(700, 600)
+//   canvas.parent('canvasForHTML')
 
-  video = createCapture(VIDEO)
-  video.hide()
-  background(0)
-  mobilenet = ml5.featureExtractor('MobileNet', { numLabels: 7 }, modelReady)
-  classifier = mobilenet.classification(video, videoReady)
+//   video = createCapture(VIDEO)
+//   video.hide()
+//   background(0)
+//   mobilenet = ml5.featureExtractor('MobileNet', { numLabels: 7 }, modelReady)
+//   classifier = mobilenet.classification(video, videoReady)
 
-  // Detect image
-  detector.then((rs) => rs.detect(video, detectionResults))
-}
+//   // Detect image
+//   detector.then((rs) => rs.detect(video, detectionResults))
+// }
 
-function draw() {
-  image(video, 0, 0, 700, 600)
-  fill(173, 255, 47)
-  textSize(30)
-  text(label, 10, height - 20)
+// function draw() {
+//   image(video, 0, 0, 700, 600)
+//   fill(173, 255, 47)
+//   textSize(30)
+//   text(label, 10, height - 20)
 
-  results.forEach((res) => {
-    stroke(255, 255, 0)
-    strokeWeight(3)
-    noFill(0)
-    rect(res.x, res.y, res.width, res.height)
-    noStroke()
-    fill(255)
-    textSize(24)
-    text(res.label, res.x + 12, res.y + 24)
-  })
-}
+//   results.forEach((res) => {
+//     stroke(255, 255, 0)
+//     strokeWeight(3)
+//     noFill(0)
+//     rect(res.x, res.y, res.width, res.height)
+//     noStroke()
+//     fill(255)
+//     textSize(24)
+//     text(res.label, res.x + 12, res.y + 24)
+//   })
+// }
